a815745b4f91bdc4fa4bfa9be9041f735928cca5
张飞 <zfeimail@163.com>
2020-08-22 10:01:41 +0800

    资源管理

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
     const element = document.createElement('div');
   
     // lodash（目前通过一个 script 引入）
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello 你好', 'webpack'], ' ');
+    element.classList.add('hello');
   
+    // 将图像添加到我们已经存在的 div 中
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    // element.appendChild(myIcon);
+
+    return element;
+  }
+
+  function component2() {
+    const element = document.createElement('div');
+  
+    // lodash（目前通过一个 script 引入）
+    element.innerHTML = _.join(['Hello 你好 㗉㱘关闭昂面', 'webpack'], ' ');
+    element.classList.add('hello2');
+  
+    // 将图像添加到我们已经存在的 div 中
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    // element.appendChild(myIcon);
+
     return element;
   }
   
   document.body.appendChild(component());
+  document.body.appendChild(component2());
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,41 @@ const path = require('path');
 module.exports = {
     entry:'./src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: [
+                    'file-loader',
+                    {
+                        loader:'image-webpack-loader',
+                        options: {
+                            bypassOndebug: true,
+                            disable: true,
+                            pngquant: {
+                                quality:[0.5,0.5],
+                                speed: 4
+                            }
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 }
